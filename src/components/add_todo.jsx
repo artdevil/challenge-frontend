27cfbacea53c3f5b7ee8a24029e2a24f13642945
adhiguna utama sabril <adhiguna.sabril@gmail.com>
2017-10-07 14:02:22 +0700

    update for compose

--- src/components/add_todo.jsx
+++ src/components/add_todo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from 'react-md/lib/TextFields';
-import { gql, graphql } from 'react-apollo';
-
-import { TodosListQuery } from './todos';
+import { graphql } from 'react-apollo';
+import queries from '../queries/todos';
+import mutations from '../mutations/todos';
 
 const AddTodo = ({ mutate }) => {
   const handleKeyUp = (e) => {
@@ -20,11 +20,11 @@ const AddTodo = ({ mutate }) => {
         },
         update: (store, { data: { add } }) => {
           // Read the data from the cache for this query.
-          const data = store.readQuery({ query: TodosListQuery });
+          const data = store.readQuery({ query: queries.TodosListQuery });
           // Add our channel from the mutation to the end.
           data.todos.push(add);
           // Write the data back to the cache.
-          store.writeQuery({ query: TodosListQuery, data });
+          store.writeQuery({ query: queries.TodosListQuery, data });
         },
       });
       e.target.value = '';
@@ -36,18 +36,8 @@ const AddTodo = ({ mutate }) => {
   );
 };
 
-const addTodoMutation = gql`
-  mutation add($title: String!) {
-    add(title: $title) {
-      id
-      title
-      completed
-    }
-  }
-`;
-
 AddTodo.propTypes = {
   mutate: PropTypes.func.isRequired,
 };
 
-export default graphql(addTodoMutation)(AddTodo);
+export default graphql(mutations.addTodoMutation)(AddTodo);
--- src/components/edit_todo.jsx
+++ src/components/edit_todo.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { gql, graphql } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import PropTypes from 'prop-types';
 import Button from 'react-md/lib/Buttons/Button';
 import DialogContainer from 'react-md/lib/Dialogs';
 import TextField from 'react-md/lib/TextFields';
 import _ from 'lodash';
-import { TodosListQuery } from './todos';
+import queries from '../queries/todos';
+import mutations from '../mutations/todos';
 
 class SimpleModal extends Component {
   static propTypes = {
@@ -73,17 +74,7 @@ class SimpleModal extends Component {
   }
 }
 
-const EditTodoMutation = gql`
-  mutation save($id: String!, $title: String!) {
-    save(id: $id, title: $title) {
-      id
-      title
-      completed
-    }
-  }
-`;
-
-const editTodo = graphql(EditTodoMutation, {
+const editTodo = graphql(mutations.EditTodoMutation, {
   props: ({ mutate }) => ({
     editTodo: (obj, text) => {
       mutate({
@@ -98,14 +89,14 @@ const editTodo = graphql(EditTodoMutation, {
         },
         update: (store, { data: { save } }) => {
           // Read the data from the cache for this query.
-          const data = store.readQuery({ query: TodosListQuery });
+          const data = store.readQuery({ query: queries.TodosListQuery });
           // Add our channel from the mutation to the end.
           const index = _.findIndex(data.todos, { id: save.id });
           const dataSelect = data.todos[index];
           dataSelect.title = save.title;
           data.todos.splice(index, 1, dataSelect);
           // Write the data back to the cache.
-          store.writeQuery({ query: TodosListQuery, data });
+          store.writeQuery({ query: queries.TodosListQuery, data });
         },
       });
     },
